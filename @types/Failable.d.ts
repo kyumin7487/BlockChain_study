55b7296554c9db835c2c2a7938ae09b6958dd58c
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-05-18 23:27:59 +0900

    :bulb: :: 주석추가

--- @types/Failable.d.ts
+++ @types/Failable.d.ts
@@ -5,3 +5,6 @@ declare type Result<R> = {isError: false; value: R};
 declare type Failure<E> = {isError: true; value: E};
 
 declare type Failable<R, E> = Result<R> | Failure<E>
+
+// 해당 타입들은 추후에 블록이 생성될 때 올바르게 생성된 블록인지를 검증하기 위한 용도 입니다.
+// TypeScript의 generic을 사용하여 Result타입 혹은 Failure 타입으르 가질 수 있는 Failable타입을 만들어준 것입니다.
